7aa6483e93780b13cac153b2b03dbd426bcf5a2c
Kleo G <theaioperator07@gmail.com>
2026-09-11 10:08:05 +0200

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter_Tight } from "next/font/google";
+import Script from "next/script";
 import { BookingProvider } from "@/components/booking/booking-provider";
 import { LanguageProvider } from "@/components/language-provider";
 import { cookies } from "next/headers";
@@ -50,7 +51,34 @@ export default async function RootLayout({
   const language = (await cookies()).get(languageCookie)?.value === "en" ? "en" : "sq";
   return (
     <html lang={language}>
-      <body className={interTight.variable}><LanguageProvider initialLanguage={language}><BookingProvider>{children}</BookingProvider></LanguageProvider></body>
+      <body className={interTight.variable}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2047032015931800');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2047032015931800&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        <LanguageProvider initialLanguage={language}>
+          <BookingProvider>{children}</BookingProvider>
+        </LanguageProvider>
+      </body>
     </html>
   );
 }
